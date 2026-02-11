90b6a48058c7a4fcd6c8619e8f817657ce7f39f0
DICLA <dicla_correia@hotmail.com>
2026-02-11 19:12:43 -0300

    Separando funcionalidades em métodos

--- AprendendoCSharp/Program.cs
+++ AprendendoCSharp/Program.cs
@@ -1,10 +1,6 @@
-﻿//int usado para números inteiros e números negativos, e a estrutura correta é colocar o tipo, o nomeDaVariavel = valor// 
-int idade = 40;
-Console.WriteLine(idade); //coloca ;- ponto e vírgula no final das linhas) (Console.WriteLine() para mostrar algo na tela.//
-//----------------------------------//
-decimal preco = 15.50m; //sempre colocar o sufixo m para indicar que é um decimal, caso contrário o compilador irá interpretar como double e pode causar erros de precisão//
-Console.WriteLine(preco);
-//----------------------------------//
+﻿VerificarIdade();
+
+VerificarPreco();
 //Só pode ser: true ou false//
 bool estaChovendo = true;
 Console.WriteLine(estaChovendo); //(No C#, essa característica de diferenciar maiúsculas de minúsculas chama-se Case Sensitivity.A primeira letra de todas as palavras é maiúscula. Onde usar: Nomes de Classes, Métodos (ações) e Arquivos.Exemplo: Console.WriteLine(), MinhaClasse, CalcularSoma().//
@@ -74,5 +70,20 @@ string[] nomes = { "Filipe", "Dicla", "Júlia", "Gabriela" }
 foreach (string nome1 in nomes) //percorre uma lista de itens do começo ao fim, sem precisar de contadores, pega um item por vez da lista.//
 {
     Console.WriteLine(nome1);
+}
+
+static void VerificarIdade()
+{
+    //int usado para números inteiros e números negativos, e a estrutura correta é colocar o tipo, o nomeDaVariavel = valor// 
+    int idade = 40;
+    Console.WriteLine(idade); //coloca ;- ponto e vírgula no final das linhas) (Console.WriteLine() para mostrar algo na tela.//
+                              //----------------------------------//
+}
+
+static void VerificarPreco()
+{
+    decimal preco = 15.50m; //sempre colocar o sufixo m para indicar que é um decimal, caso contrário o compilador irá interpretar como double e pode causar erros de precisão//
+    Console.WriteLine(preco);
+    //----------------------------------//
 }
 //----------------------------------//
